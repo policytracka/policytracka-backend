4c0ceb09e9723370b69a7741c2290311d4d732ab
hrnph <hirunkul2548@gmail.com>
2023-03-12 16:18:59 +0700

    add model and views sample

--- policytracka_api/backend/models.py
+++ policytracka_api/backend/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+# Create your models here. ฐานข้อมูล
+class Policydata(models.Model):
+    title=models.CharField(max_length=255,unique=True) #การบรรจุข้อความได้สูงสุดประมาณ 255ตัวอักษร และตรวจจับชื่อและรหัสที่ซ้ำกัน
+    detail=models.SlugField(max_length=5000,unique=False)
+    policy_owner=models.CharField(max_length=255,unique=False)
+    
+    def __str__(self): #บ่งบอกหมวดหมู่ Category
+        return self.name 
+    
+    # class Meta : #การตั้งค่าภาษาไทยหน้า admin 
+    #     ordering=('name',) #การเรียงตัวอักษร
+    #     verbose_name='หมวดหมู่สินค้า'
+    #     verbose_name_plural="ข้อมูลประเภทสินค้า"
